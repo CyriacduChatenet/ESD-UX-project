9152111fe3cb6714fa825f07665408f14ca022ce
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-04-25 17:47:02 +0200

    [FIIX] page

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom";
 import { App } from "./app/App";
 import { UserProvider } from "./setup/context/user.context";
 import { ScoreProvider } from "./setup/context/score.context";
+import { AnswerProvider } from "./setup/context/answer.context";
 
 ReactDOM.render(
   <UserProvider>
     <ScoreProvider>
-      <App />
+      <AnswerProvider>
+        <App />
+      </AnswerProvider>
     </ScoreProvider>
   </UserProvider>,
   document.querySelector("#root")
